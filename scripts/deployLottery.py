1349c73d161cd2d2e8b4fb6afa88495a8b6e05f2
ReasonZx <xico.reasonzx@gmail.com>
2023-02-19 23:57:54 +0100

    Changes to deploy SC script

--- scripts/deployLottery.py
+++ scripts/deployLottery.py
@@ -7,11 +7,11 @@ def deployLottery():
     account = helpfulScripts.getAccount()
     
     lottery_contract = lottery.deploy(
-        helpfulScripts.getContractAddress(account, "eth_usd_price_feed"),
-        helpfulScripts.getContractAddress(account, "vrfCoordinator"),
-        helpfulScripts.getContractAddress(account, "linkToken"),
-        helpfulScripts.getContractAddress(account, "keyHash"),
-        helpfulScripts.getContractAddress(account, "fee"),
+        helpfulScripts.getContractAddress("eth_usd_price_feed", account),
+        helpfulScripts.getContractAddress("vrfCoordinator", account),
+        helpfulScripts.getContractAddress("linkToken", account),
+        helpfulScripts.getContractAddress("keyHash", account),
+        helpfulScripts.getContractAddress("fee", account),
         {'from': account},
     )
     return lottery_contract
--- scripts/helpfulScripts.py
+++ scripts/helpfulScripts.py
@@ -1,4 +1,4 @@
-from brownie import accounts, network, config, MockV3Aggregator
+from brownie import accounts, network, config, MockV3Aggregator, Contract, LinkToken
 
 def getAccount():
     if network.show_active() == "development" or ("fork" in network.show_active()):
@@ -6,9 +6,33 @@ def getAccount():
     else:
         return accounts.add(config["wallets"]["from_key"])
 
-def getContractAddress(_account, _contractName):
+def getContractAddress(_contractName, _account=None):
     if network.show_active() == "development":
         mockAggregator = MockV3Aggregator.deploy(18, 2000000000000000000000, {"from": _account})
         return mockAggregator.address
     else:
         return config["networks"][network.show_active()][_contractName]
+
+def getContract(_contractType, _account=None):
+    SCaddress = getContractAddress(_contractName, _account)
+    if network.show_active() == "development":
+        print("Functionality not available in development network. Use testnet or local fork.")
+        exit()
+    else:
+        contract = Contract.from_abi(_contractType.name, SCaddress, _contractType.abi)
+    
+    return contract 
+
+
+
+def fundSC (SCadress, account=None, token=None, ammount = 100000000000000000):      #0.1
+    if(not account):
+        account = getAccount()
+    if(not token):
+        token = getContract(LinkToken)
+
+    tx = token.transfer(SCadress, ammount, {"from": account})
+    tx.wait(1)
+    
+    print("Contracted funded!")
+    return tx
